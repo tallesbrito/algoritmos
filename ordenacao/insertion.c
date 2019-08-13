70d1ce674c8a101d96c5e0b1d2dc7f052c0e8315
tallesbrito <tallesbrito@gmail.com>
2019-08-13 18:31:02 -0300

    Insertion sort implementation was added.

---
+++ ordenacao/insertion.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void insertion_sort(int A[], int n) { 
+  int i, j, chave;
+  for (i = 1; i < n; i++){   
+     chave = A[i];
+     j = i-1;
+
+     while (j>=0 && A[j]>chave){
+        A[j+1] = A[j];
+        j--;       
+     }
+     
+     A[j+1] = chave;
+  }
+}
+
+
+int main(){
+
+  int vet[] = {50,40,20,30,10};
+
+  insertion_sort(vet,5);
+
+  int i;
+  for(i=0;i<5;i++)
+    printf("%d ", vet[i]);
+
+  printf("\n");
+
+  return 0;
+}
